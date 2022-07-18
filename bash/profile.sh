e19d6b2c80bd5e1867142efaa884ee1f17a8788f
maximilionus <maximilionuss@gmail.com>
2022-07-18 05:47:48 +0300

    bash: `video_rescale` fixed additional args handle

--- bash/profile.sh
+++ bash/profile.sh
@@ -6,7 +6,7 @@ function video_rescale () {
     local desired_framerate=${FRM:=30}
     local desired_quality=${QLT:=28}
     local volume_multiplier=${VLM:=1}
-    local additional_args="${ARG}"
+    local additional_args=${ARG}
 
     local output_path="${source_video_path}_${desired_resolution}_${desired_framerate}.mp4"
 
@@ -17,7 +17,7 @@ function video_rescale () {
     fi
 
     local ffmpeg_params=("-s" "${desired_resolution}" "-filter:v" "fps=${desired_framerate}" "-c:v" "libx264" "-crf" "${desired_quality}")
-    ffmpeg_params+=("${audio_args[@]}""${additional_args}")
+    ffmpeg_params+=("${audio_args[@]}""${additional_args[@]}")
 
     local final_args=("-i" "$source_video_path" "${ffmpeg_params[@]}" "$output_path")
 
