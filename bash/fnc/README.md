e28c7f666d5263840cb95e93a5674116a55023f7
maximilionus <maximilionuss@gmail.com>
2022-08-26 16:25:01 +0300

    Bash: split all functions to scripts, partly done
    'video_rescale' script works at basic level without optional arguments parsing. Will be fixed in next commits
    
    `vactiva` script not tested yet, but should work fine
    
    All unix files line format converted to LF from CRLF

---
+++ bash/fnc/README.md
@@ -0,0 +1,4 @@
+# Bash Functions
+
+## About
+All custom bash functions located here for easy access from any shell *(Including not POSIX-based)*. This path should be added to your `$PATH` variable to work properly.
--- bash/profile.sh
+++ bash/profile.sh
@@ -1,38 +1,4 @@
 #!/bin/bash
 
-function video_rescale () {
-    local source_video_path="${1}"
-    local desired_resolution="${RES:=1280x720}"
-    local desired_framerate=${FRM:=30}
-    local desired_quality=${QLT:=28}
-    local volume_multiplier=${VLM:=1}
-    local additional_args="${ARG}"
-
-    local output_path="${source_video_path}_${desired_resolution}_${desired_framerate}.mp4"
-
-    if [ "$volume_multiplier" = "1" ]; then
-        local audio_args=("-c:a" "copy")
-    else
-        local audio_args=("-c:a" "aac" "-filter:a" "volume=${volume_multiplier}")
-    fi
-
-    local ffmpeg_params=("-s" "${desired_resolution}" "-filter:v" "fps=${desired_framerate}" "-c:v" "libx264" "-crf" "${desired_quality}")
-    ffmpeg_params+=("${audio_args[@]}""${additional_args}")
-
-    local final_args=("-i" "$source_video_path" "${ffmpeg_params[@]}" "$output_path")
-
-    ffmpeg ${final_args}
-}
-
-function vactiva () {
-    if [[ ! -d ".venv" ]] ; then
-        virtualenv .venv
-    fi;
-
-    case "$(uname -s)" in
-        Linux*|Darwin*) source ".venv/bin/activate";;
-        *)              source ".venv/Scripts/activate";;
-    esac;
-
-    printf "Python Virtual Environment - Activated\nPYTHON: %s\n" "$(which python)"
-}
+# Include all custom functions to path for easy access
+PATH=$PATH:./fnc
