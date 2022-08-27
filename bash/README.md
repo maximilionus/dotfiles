7473f0b0586b9e1294b2d9e0ef3770d425f31f11
maximilionus <maximilionuss@gmail.com>
2022-08-27 11:52:29 +0300

    Bash: removed 'profile.sh', video_rescale enhanced
    'video_rescale' script now will show help message if main argument with video path is not provided

--- bash/README.md
+++ bash/README.md
@@ -1,4 +1,1 @@
 # Bash Configuration
-
-## How To Use
-- Simply source this script from main `.*rc` file of the shell
--- bash/profile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Include all custom functions to path for easy access
-PATH=$PATH:./bin  # Fix: path should be absolute
