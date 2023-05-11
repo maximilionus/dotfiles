cf3d5d14045607fe2242c980bbf1dbbfb3a6785e
maximilionus <maximilionuss@gmail.com>
2023-05-11 03:41:01 +0300

    `mpv`: Add config with install script

---
+++ mpv/install.py
@@ -0,0 +1,35 @@
+"""
+MPV personal config installation script.
+Run the script with elevated privileges or ensure that user is allowed to create symlinks!
+
+Supported platforms: Linux, macOS, Windows
+
+How to run:
+$ python install.py
+"""
+
+import platform
+from pathlib import Path
+
+
+source_dir = Path(__file__).parent
+current_platform = platform.system()
+
+if current_platform in ('Linux', 'Darwin'):
+    link_target = Path.home() / Path(".config/mpv")
+elif current_platform == 'Windows':
+    link_target = Path.home() / Path("AppData/Roaming/mpv")
+else:
+    print("Platform is not supported")
+    exit()
+
+if link_target.exists():
+    print("Configuration already installed in '{}'".format(link_target))
+    exit()
+
+
+try:
+    link_target.symlink_to(source_dir)
+    print("Successfully created the symlink to '{}' in '{}'".format(source_dir, link_target))
+except Exception:
+    print("Can not create the link. Run the script with elevated privileges or ensure that user is allowed to create symlinks.")
