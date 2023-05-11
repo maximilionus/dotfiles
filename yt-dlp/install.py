059a49994a201c09f4119cc302e2aab96038875b
maximilionus <maximilionuss@gmail.com>
2023-05-11 03:41:40 +0300

    `yt-dlp`: Improve install script, add docs

--- yt-dlp/install.py
+++ yt-dlp/install.py
@@ -1,21 +1,31 @@
+"""
+yt-dlp personal config installation script.
+Run the script with elevated privileges or ensure that user is allowed to create symlinks!
+
+Supported platforms: Linux, macOS, Windows
+
+How to run:
+$ python install.py
+"""
+
 from pathlib import Path
 
 
-CONFIG_DIR: Path = Path.home() / Path(".config")
-SOURCE = Path(__file__).parent / Path('yt-dlp.conf')
-LINK = CONFIG_DIR / Path('yt-dlp.conf')
+config_dir = Path.home() / Path(".config")
+source_file = Path(__file__).parent / Path('yt-dlp.conf')
+target_file = config_dir / Path('yt-dlp.conf')
 
 
-if not CONFIG_DIR.exists():
-    CONFIG_DIR.mkdir()
-    print("Created '{}'".format(CONFIG_DIR))
+if not config_dir.exists():
+    config_dir.mkdir()
+    print("Created the '{}' dir".format(config_dir))
 
-if LINK.exists():
-    print("Configuration file already installed in '{}'".format(LINK))
+if target_file.exists():
+    print("Configuration file already installed in '{}'".format(target_file))
     exit()
 
 try:
-    LINK.symlink_to(SOURCE)
-    print("Successfully created the symlink to '{}' in '{}'".format(SOURCE, LINK))
+    target_file.symlink_to(source_file)
+    print("Successfully created the symlink to '{}' in '{}'".format(source_file, target_file))
 except Exception:
-    print("Can not create the link. Run script with elevated privileges or ensure that user is allowed to create symlinks.")
+    print("Can not create the link. Run the script with elevated privileges or ensure that user is allowed to create symlinks.")
