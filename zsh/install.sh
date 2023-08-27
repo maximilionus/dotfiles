975e9ffb675fcaab8babee76f30614d6b5f35dd7
maximilionus <maximilionuss@gmail.com>
2023-08-28 00:07:31 +0300

    zsh: Add config and install script

---
+++ zsh/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Install antigen
+echo ": Setting up the dependencies."
+curl --create-dirs -L git.io/antigen -o "$HOME/.zsh/antigen.zsh"
+
+# Link the config
+target_path=$(realpath "./zshrc")
+link_path="$HOME/.zshrc"
+
+ln -sfv $target_path $link_path
+
+echo "+ ZSH configuration successfully installed."
+echo "  Restart your shell to load the changes."
