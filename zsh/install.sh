4e396e7b9022e4927781742830df257f97e65258
maximilionus <maximilionuss@gmail.com>
2023-12-24 02:40:26 +0300

    zsh: Enhance install script syntax

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,12 +4,11 @@ set -e
 
 echo "=> Setting up the dependencies."
 echo -n "--> Installing the Antigen package manager for ZSH: "
-curl --progress-bar --create-dirs \
-  -L git.io/antigen \
-  -o "$HOME/.zsh/antigen.zsh"
+curl --progress-bar --create-dirs -L \
+    -o ~/.zsh/antigen.zsh \
+    git.io/antigen
 
 echo "=> Installing the configuration."
-
 target_path=$(realpath "./zshrc")
 link_path="$HOME/.zshrc"
 echo -n "--> Linking the ZSH config: "
