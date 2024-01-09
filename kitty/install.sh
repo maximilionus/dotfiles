a25f0a3e70b1fe86dfbc7657dba70a3767657aed
maximilionus <maximilionuss@gmail.com>
2024-01-09 23:54:45 +0300

    Kitty: Add basic configuration

---
+++ kitty/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+KITTY_CONFIG_DIR="$HOME/.config/kitty"
+
+echo "-> Preparing the configuration paths"
+mkdir -p "$KITTY_CONFIG_DIR"
+
+echo "-> Installing the configuration."
+echo "==> Linking the Kitty config:"
+ln -sfv $(realpath "./kitty.conf") $KITTY_CONFIG_DIR
+echo "==> Linking the Kitty theme:"
+ln -sfv $(realpath "./theme.conf") $KITTY_CONFIG_DIR
+
+echo "-> Kitty configuration successfully installed"
