92f6b444586c0a059c1d4b8cbde99f95e80308b4
maximilionus <maximilionuss@gmail.com>
2024-04-05 18:51:34 +0300

    kitty: Add dark-light mode themes and keybind

--- kitty/install.sh
+++ kitty/install.sh
@@ -10,7 +10,8 @@ mkdir -p "$KITTY_CONFIG_DIR"
 echo "-> Installing the configuration."
 echo "==> Linking the Kitty config:"
 ln -sfv $(realpath "./kitty.conf") $KITTY_CONFIG_DIR
-echo "==> Linking the Kitty theme:"
-ln -sfv $(realpath "./current-theme.conf") $KITTY_CONFIG_DIR
+echo "==> Linking the Kitty themes:"
+ln -sfv $(realpath "./theme-dark.conf") $KITTY_CONFIG_DIR
+ln -sfv $(realpath "./theme-light.conf") $KITTY_CONFIG_DIR
 
 echo "-> Kitty configuration successfully installed"
