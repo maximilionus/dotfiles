b8ab4478c83f15cfafab815cfee411b66cb2710f
maximilionus <maximilionuss@gmail.com>
2024-04-28 21:38:30 +0300

    alacritty: Add config with some basic tweaks

---
+++ alacritty/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+CONFIG_DIR="$HOME/.config/alacritty"
+
+echo "-> Preparing the configuration paths"
+mkdir -p "$CONFIG_DIR"
+
+echo "-> Installing the configuration."
+echo "==> Linking the Alacritty config:"
+ln -sfv $(realpath "./alacritty.toml") $CONFIG_DIR
+
+echo "-> Alacritty configuration successfully installed"
