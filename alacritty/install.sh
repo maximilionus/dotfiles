fbb57de598e036a349e1a34fd6eae46a10d6865b
maximilionus <maximilionuss@gmail.com>
2024-04-29 02:31:03 +0300

    alacritty: Add custom theme

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -10,5 +10,7 @@ mkdir -p "$CONFIG_DIR"
 echo "-> Installing the configuration."
 echo "==> Linking the Alacritty config:"
 ln -sfv $(realpath "./alacritty.toml") $CONFIG_DIR
+echo "==> Linking the themes:"
+ln -sfv $(realpath "./theme.toml") $CONFIG_DIR
 
 echo "-> Alacritty configuration successfully installed"
