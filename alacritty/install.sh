d6abe1d58e6cf03215b2a9f6f419688cd96844c5
maximilionus <maximilionuss@gmail.com>
2024-05-14 03:40:45 +0300

    alacritty: Modify config for new structure

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -3,15 +3,25 @@
 set -e
 
 CONFIG_DIR="$HOME/.config/alacritty"
+SCRIPTS_DIR="$HOME/.local/bin"
 
 echo "-> Preparing the configuration paths"
-mkdir -p "$CONFIG_DIR"
+mkdir -p $CONFIG_DIR
+mkdir -p $SCRIPTS_DIR
 
 echo "-> Installing the configuration."
+
 echo "==> Linking the Alacritty config:"
 ln -sfv $(realpath "./alacritty.toml") $CONFIG_DIR
+
 echo "==> Linking the themes:"
 ln -sfv $(realpath "./theme_dark.toml") $CONFIG_DIR
 ln -sfv $(realpath "./theme_light.toml") $CONFIG_DIR
+cd $CONFIG_DIR
+ln -sfv ./theme_dark.toml theme_selected.toml
+cd -
+
+echo "==> Linking the scripts:"
+ln -sfv $(realpath "./scripts/switch_theme.sh") $SCRIPTS_DIR
 
 echo "-> Alacritty configuration successfully installed"
