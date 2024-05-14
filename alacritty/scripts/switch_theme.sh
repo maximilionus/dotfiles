46071b014cce9b85105ff99d81a3eb4fa647f389
maximilionus <maximilionuss@gmail.com>
2024-05-14 03:59:35 +0300

    alacritty: New, fully working theme switcher

--- alacritty/scripts/switch_theme.sh
+++ alacritty/scripts/switch_theme.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-sed -i --follow-symlinks 's/dark.toml/light.toml/' ~/.config/alacritty/alacritty.toml && exit 0
-sed -i --follow-symlinks 's/light.toml/dark.toml/' ~/.config/alacritty/alacritty.toml
+CONFIG_DIR="$HOME/.config/alacritty"
+THEME_SELECTED="theme_selected.toml"
+THEME_DARK="theme_dark.toml"
+THEME_LIGHT="theme_light.toml"
+
+if [ ! -d "$CONFIG_DIR" ]; then
+    echo "Alacritty configuration not found."
+    exit 1
+fi
+
+cd "$CONFIG_DIR"
+if [ "$THEME_SELECTED" -ef "$THEME_DARK" ]; then
+    echo "Switching Alacritty theme to the light mode."
+    ln -sfv "./$THEME_LIGHT" "$THEME_SELECTED"
+else
+    echo "Switching Alacritty theme to the dark mode."
+    ln -sfv "./$THEME_DARK" "$THEME_SELECTED"
+fi
