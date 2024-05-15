1a55f2d3793cecd6cb5a6aaffc95b07a80b06847
maximilionus <maximilionuss@gmail.com>
2024-05-15 15:41:46 +0300

    nvim: Follow gnome color scheme, add Catppuccin

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,1 +1,9 @@
-vim.cmd.colorscheme "moonfly"
+local gnome_prefers = vim.fn.system {'gsettings', 'get', 'org.gnome.desktop.interface', 'color-scheme'}
+
+if string.find(gnome_prefers, 'default') then
+    -- Light
+    vim.cmd.colorscheme "catppuccin-latte"
+else
+    -- Dark
+    vim.cmd.colorscheme "moonfly"
+end
