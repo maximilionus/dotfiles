3aaf6abf3c8fa5663941f7088d8e2d5653f13475
maximilionus <maximilionuss@gmail.com>
2024-05-15 18:21:44 +0300

    nvim: Enhance theme.lua syntax

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,9 +1,13 @@
-local _, gnome_prefers = pcall(vim.fn.system, {'gsettings', 'get', 'org.gnome.desktop.interface', 'color-scheme'})
+local _, gnome_prefers = pcall(
+    vim.fn.system,
+    {'gsettings', 'get', 'org.gnome.desktop.interface', 'color-scheme'}
+)
 
 if string.find(gnome_prefers, 'default') then
     -- Light
     vim.cmd.colorscheme "catppuccin-latte"
 else
     -- Dark
+    -- Also used as a fallback for environments other than Gnome
     vim.cmd.colorscheme "moonfly"
 end
