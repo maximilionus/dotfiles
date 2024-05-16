33e1ac7eda4bc7f32d167685c9384cfcf8b860a3
maximilionus <maximilionuss@gmail.com>
2024-05-17 00:12:04 +0300

    vim,nvim: Change leader key to 'space'

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
+-- Configure leader key
+vim.g.mapleader = " "
+
 -- Bootstrap plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -1,5 +1,5 @@
 -- Unhighlight search
-vim.keymap.set('n', '<Leader><Space>', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader><Space>', ':nohlsearch<CR>')
 
 -- Buffer Navigation
 --- Some actions split to Telescope
@@ -8,8 +8,8 @@ vim.keymap.set('n', '<S-Tab>', ':bprev<CR>')
 vim.keymap.set('n', 'Q', ':bd!<CR>')
 
 -- Spelling check switches
-vim.keymap.set('n', '<Leader>l', ':set spell spelllang=en_us')
-vim.keymap.set('n', '<Leader><S-l>', ':set nospell<CR>')
+vim.keymap.set('n', '<leader>l', ':set spell spelllang=en_us')
+vim.keymap.set('n', '<leader><S-l>', ':set nospell<CR>')
 
 -- Telescope keybinds
 local builtin = require('telescope.builtin')
