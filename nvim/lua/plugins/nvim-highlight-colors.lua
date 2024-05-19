50009543c2dfe1053cf3fbd4ab802baf33cd09d7
maximilionus <maximilionuss@gmail.com>
2024-05-19 15:22:11 +0300

    nvim: Enhance color plugin config

--- nvim/lua/plugins/nvim-highlight-colors.lua
+++ nvim/lua/plugins/nvim-highlight-colors.lua
@@ -2,9 +2,6 @@ return {
     'brenoprata10/nvim-highlight-colors',
     opts = {
         render = 'virtual',
-        virtual_symbol = '●',
-    },
-    setup = function(PluginSpec, opts)
-        require('nvim-highlight-colors').setup(args)
-    end
+        virtual_symbol = '●'
+    }
 }
