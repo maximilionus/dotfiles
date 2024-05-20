c38a24157f99f81ec9e41018c0698325b6ef7741
maximilionus <maximilionuss@gmail.com>
2024-05-20 20:24:41 +0300

    nvim: Enforce HJKL

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -16,3 +16,9 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>o', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+
+-- Make the normie suffer, enforce HJKL 😈
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
