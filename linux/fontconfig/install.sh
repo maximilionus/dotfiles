7877d7da22a7b518aad7076a00885b024885fdf4
maximilionus <maximilionuss@gmail.com>
2024-05-22 18:39:55 +0300

    linux/fontconfig: Prefer color emoji, new linker

--- linux/fontconfig/install.sh
+++ linux/fontconfig/install.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-echo "-> Prepare the directories."
-mkdir -p -v ~/.config/fontconfig
+FONTCONFIG_USER=~/.config/fontconfig
 
-echo "-> Installing the configuration."
-cp -rv conf.d ~/.config/fontconfig
+
+echo "-> Prepare the directories."
+mkdir -p -v $FONTCONFIG_USER
+
+echo "-> Linking the configuration."
+ln -svf $(realpath ./conf.d) $FONTCONFIG_USER
 
 echo "-> fontconfig user configuration successfully installed."
 echo "   Execute 'fc-cache -f' if cache is not updated automatically."
