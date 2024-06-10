6d7e2d7410a00d73d7b42a0320cbf33fe68d173e
maximilionus <maximilionuss@gmail.com>
2024-06-11 00:55:51 +0300

    alacritty: Refactor theme switching script

--- alacritty/scripts/user/alacritty-theme-switch.sh
+++ alacritty/scripts/user/alacritty-theme-switch.sh
@@ -7,40 +7,51 @@ THEME_LIGHT="theme_light.toml"
 
 
 set_dark () {
-    echo "Switching Alacritty theme to the dark mode."
+    cd "$CONFIG_DIR"
+
+    if [ "$THEME_SELECTED" -ef "$THEME_DARK" ]; then
+        echo "Theme is already set to dark."
+        exit 0
+    fi
+
     ln -sf "./$THEME_DARK" "$THEME_SELECTED"
+    echo "Switched Alacritty theme to dark mode."
 }
 
 set_light () {
-    echo "Switching Alacritty theme to the light mode."
+    cd "$CONFIG_DIR"
+
+    if [ "$THEME_SELECTED" -ef "$THEME_LIGHT" ]; then
+        echo "Theme is already set to light."
+        exit 0
+    fi
+
+    echo "Switched Alacritty theme to light mode."
     ln -sf "./$THEME_LIGHT" "$THEME_SELECTED"
 }
 
+set_autoswitch () {
+    cd "$CONFIG_DIR"
 
-if [ ! -d "$CONFIG_DIR" ]; then
-    echo "Alacritty configuration not found."
-    exit 1
-fi
-
-cd "$CONFIG_DIR"
-
-if [ -z "$1" ]; then
-    # Auto theme switcher
     if [ "$THEME_SELECTED" -ef "$THEME_DARK" ]; then
         set_light
     else
         set_dark
     fi
-else
-    # Switch to provided value
-    if [ "$1" == "light" ]; then
-        if [ "$THEME_SELECTED" -ef "$THEME_DARK" ]; then
-            set_light
-        fi
-    elif [ "$1" == "dark" ]; then
-        if [ "$THEME_SELECTED" -ef "$THEME_LIGHT" ]; then
-            set_dark
-        fi
-    fi
+}
 
+
+# Main
+if [ ! -d "$CONFIG_DIR" ]; then
+    echo "Alacritty configuration not found."
+    exit 1
 fi
+
+if [ -z "$1" ]; then
+    set_autoswitch
+elif [ "$1" == "light" ]; then
+    set_light
+elif [ "$1" == "dark" ]; then
+    set_dark
+fi
+
