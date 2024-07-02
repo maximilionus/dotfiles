b2705d5527776f85c5c03d8243750f2aacc9fb38
maximilionus <maximilionuss@gmail.com>
2024-07-02 20:27:50 +0300

    nvim: Disable mini.statusline

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -4,7 +4,6 @@ return {
     config = function()
         require('mini.align').setup()      -- Align text with 'ga'
         require('mini.comment').setup()    -- Comment code with 'gc'
-        require('mini.statusline').setup() -- Minimal status line
         require('mini.trailspace').setup() -- Trail whitespace (Used in keybindings)
     end,
 }
