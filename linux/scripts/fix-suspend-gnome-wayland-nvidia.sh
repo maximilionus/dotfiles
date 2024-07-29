ea2f5a15a283fb8496efbc5069424d2c79f0f48e
maximilionus <maximilionuss@gmail.com>
2024-07-29 06:24:54 +0300

    linux: NVIDIA GDM wayland suspend fix
    This is the final solution of what I was able to find and experiment on.
    It should solve all the suspend issues under GNOME Wayland sessions with
    NVIDIA proprietary drivers.
    
    If this fails, I'll throw this abomination of a GPU out of my fucking
    window. Nvidia never fails to disappoint.

---
+++ linux/scripts/fix-suspend-gnome-wayland-nvidia.sh
@@ -0,0 +1,48 @@
+# Script will create 2 services to ensure that gnome-shell is suspended
+# **before** the NVIDIA driver, so it will not make any resume calls preventing
+# the correct system suspend procedure.
+
+set -e
+
+SYSTEMD_DIR="/etc/systemd/system"
+
+cat <<EOF > "$SYSTEMD_DIR/nvidia-gnome-suspend.service"
+[Unit]
+Description=Correct suspend for gnome-shell under nvidia driver
+Before=systemd-suspend.service
+Before=systemd-hibernate.service
+Before=nvidia-suspend.service
+Before=nvidia-hibernate.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/killall -STOP gnome-shell
+
+[Install]
+WantedBy=systemd-suspend.service
+WantedBy=systemd-hibernate.service
+EOF
+
+
+cat <<EOF > "$SYSTEMD_DIR/nvidia-gnome-resume.service"
+[Unit]
+Description=Correct resume for gnome-shell under nvidia driver
+After=systemd-suspend.service
+After=systemd-hibernate.service
+After=nvidia-resume.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/killall -CONT gnome-shell
+
+[Install]
+WantedBy=systemd-suspend.service
+WantedBy=systemd-hibernate.service
+EOF
+
+
+systemctl daemon-reload
+systemctl enable nvidia-gnome-suspend.service
+systemctl enable nvidia-gnome-resume.service
+
+echo "-- Fixes applied and ready, no reboot required."
