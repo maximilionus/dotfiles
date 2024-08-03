e32dccd0c95d53868375ab41e283f8d0eb3f2a5e
maximilionus <maximilionuss@gmail.com>
2024-08-03 05:09:40 +0300

    nvim: Transparent elements back

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,22 @@
 -- Configure leader key
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 -- Bootstrap plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load modules
-require("lazy").setup("plugins")
-require("options")
-require("keybindings")
+require('lazy').setup('plugins')
+require('options')
+require('keybindings')
+require('theme')
---
+++ nvim/lua/theme.lua
@@ -0,0 +1,4 @@
+-- Transparent elements
+vim.api.nvim_set_hl(0, 'Normal', { bg=none })
+vim.api.nvim_set_hl(0, 'NonText', { bg=none })
+vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
