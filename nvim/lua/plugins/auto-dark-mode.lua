029322188f6cf7034d514e7ba756aca07d11b701
maximilionus <maximilionuss@gmail.com>
2024-08-25 19:15:50 +0300

    nvim: Fix elements visibility issue

--- nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -5,10 +5,16 @@ return {
         set_dark_mode = function()
             vim.api.nvim_set_option_value("background", "dark", {})
             vim.o.background = 'dark'
+            vim.api.nvim_set_hl(0, 'Normal', { bg=none })
+            vim.api.nvim_set_hl(0, 'NonText', { bg=none })
+            vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
         end,
         set_light_mode = function()
             vim.api.nvim_set_option_value("background", "light", {})
             vim.o.background = 'light'
+            vim.api.nvim_set_hl(0, 'Normal', { bg=none })
+            vim.api.nvim_set_hl(0, 'NonText', { bg=none })
+            vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
         end,
     },
 }
--- nvim/lua/theme.lua
+++
@@ -1,4 +0,0 @@
--- Transparent elements
-vim.api.nvim_set_hl(0, 'Normal', { bg=none })
-vim.api.nvim_set_hl(0, 'NonText', { bg=none })
-vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
