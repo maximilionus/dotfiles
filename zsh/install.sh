bc166f67b92cedce559ad9cbdedf2ebf312c96ae
maximilionus <maximilionuss@gmail.com>
2024-10-12 06:35:13 +0300

    zsh: Prettify installer

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,17 +2,13 @@
 
 set -e
 
-echo "=> Setting up the dependencies."
-echo -n "--> Installing the Antigen package manager for ZSH: "
-curl --progress-bar --create-dirs -L \
-    -o ~/.zsh/antigen.zsh \
-    git.io/antigen
+echo "-> Setting up the dependencies."
+curl --create-dirs -L \
+    -o ~/.zsh/antigen.zsh git.io/antigen
 
-echo "=> Installing the configuration."
+echo "-> Installing the configuration."
 target_path=$(realpath "./zshrc")
 link_path="$HOME/.zshrc"
-echo -n "--> Linking the ZSH config: "
 ln -sfv $target_path $link_path
 
-echo "=> ZSH configuration successfully installed,"
-echo "   restart your shell to load the changes."
+echo "zsh configuration successfully installed, restart shell to load the changes."
