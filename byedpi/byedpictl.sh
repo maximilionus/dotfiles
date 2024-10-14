5238f19ee9d8a8f883a9103d6bac195f2efe14bf
maximilionus <maximilionuss@gmail.com>
2024-10-14 21:59:15 +0300

    byedpi: Replace redirect mode with full tunneling
    Implemented using the hev-socks5-tunnel.

--- byedpi/byedpictl.sh
+++ byedpi/byedpictl.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 set -e
 
-show_help() {
+cmd_help() {
     cat <<EOF
 $0
 
 COMMANDS:
-    mode [redirect,proxy]:
-        Set routing to redirect all TCP traffic to local proxy, or use it as a
-        plain SOCKS5 proxy server, requiring manual connection from user.
+    mode [tun,proxy]:
+        Set routing to tunnel all traffic to local proxy, or use it as a plain
+        SOCKS5 proxy server, requiring manual connection from user.
 
-        Default: proxy mode.
+        Default active: proxy mode.
     help:
         Show this message and exit.
 EOF
 }
 
-set_mode() {
+cmd_mode() {
     case $1 in
-        # Setting iptables rules as root is required to prevent the traffic
-        # self redirection.
-        redirect)
-            sudo iptables -t nat -A OUTPUT -p tcp -m owner ! --uid-owner root \
-                -j REDIRECT --to-port 4080
-            echo "Successfully changed the mode to redirect."
-            echo "All TCP traffic will now be automatically redirected to proxy."
+        tun)
+            sudo hev-socks5-tunnel /etc/byedpi/hev-socks5-tunnel.yaml &
+            sudo ip rule add uidrange 1001-1001 lookup 110 pref 28000 || true
+            sudo ip route add default via 192.168.1.1 dev enp9s0 metric 50 table 110 || true
+            sudo ip route add default via 172.20.0.1 dev byedpi-tun metric 1 || true
             ;;
 
         proxy)
-            sudo iptables -t nat -D OUTPUT -p tcp -m owner ! --uid-owner root \
-                -j REDIRECT --to-port 4080
+            sudo ip rule del uidrange 1001-1001 lookup 110 pref 28000
+            sudo ip route del default via 192.168.1.1 dev enp9s0 metric 50 table 110
+            sudo ip route del default via 172.20.0.1 dev byedpi-tun metric 1
+
             echo "Successfully changed the mode to proxy (default)"
             echo "Manual user connection to proxy server is required."
             ;;
+
         *)
             echo "Error: Invalid mode: \"$1\"."
             echo "Choose from \"redirect\" and \"proxy\"."
@@ -41,10 +42,10 @@ set_mode() {
 
 case $1 in
     help)
-        show_help
+        cmd_help
         ;;
     mode)
-        set_mode $2
+        cmd_mode $2
         ;;
     *)
         echo "Invalid argument $1"
--- byedpi/install.sh
+++ byedpi/install.sh
@@ -2,11 +2,23 @@
 set -e
 
 SERVICE="byedpi.service"
+CONF_DIR="/etc/byedpi"
+BIN_DIR="/usr/local/bin"
+
+echo "-> Preparing"
+mkdir -vp "$CONF_DIR"
+id -u byedpi &>/dev/null || sudo useradd -M -s /bin/false byedpi
+
+echo "-> Downloading dependencies"
+curl -L -o "$BIN_DIR/hev-socks5-tunnel" \
+    https://github.com/heiher/hev-socks5-tunnel/releases/download/2.7.4/hev-socks5-tunnel-linux-x86_64
+chmod +x "$BIN_DIR/hev-socks5-tunnel"
 
 echo "-> Installing ByeDPI configuration"
-cp -v $SERVICE /etc/systemd/system
-cp -v byedpi.conf /etc/
-cp -v byedpictl.sh /usr/local/bin/byedpictl
+cp -v "$SERVICE" /etc/systemd/system
+cp -v byedpi.conf "$CONF_DIR"
+cp -v hev-socks5-tunnel.yaml "$CONF_DIR"
+cp -v byedpictl.sh "$BIN_DIR/byedpictl"
 
 echo "-> Activating services"
-systemctl enable --now $SERVICE
+systemctl enable --now "$SERVICE"
