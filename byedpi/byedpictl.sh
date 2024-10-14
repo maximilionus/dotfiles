aab4cf3355c1a6420e78ecdbb1ba9627e80c3111
maximilionus <maximilionuss@gmail.com>
2024-10-14 22:04:35 +0300

    byedpi: Replace mode command with tun

--- byedpi/byedpictl.sh
+++ byedpi/byedpictl.sh
@@ -6,26 +6,30 @@ cmd_help() {
 $0
 
 COMMANDS:
-    mode [tun,proxy]:
-        Set routing to tunnel all traffic to local proxy, or use it as a plain
-        SOCKS5 proxy server, requiring manual connection from user.
+    tun <bool>:
+        Set routing to tunnel all traffic to local proxy, or use a plain SOCKS5
+        proxy server, requiring manual connection from user.
 
-        Default active: proxy mode.
+        Default: false.
     help:
         Show this message and exit.
 EOF
 }
 
-cmd_mode() {
+cmd_tun() {
     case $1 in
-        tun)
+        true)
             sudo hev-socks5-tunnel /etc/byedpi/hev-socks5-tunnel.yaml &
             sudo ip rule add uidrange 1001-1001 lookup 110 pref 28000 || true
             sudo ip route add default via 192.168.1.1 dev enp9s0 metric 50 table 110 || true
             sudo ip route add default via 172.20.0.1 dev byedpi-tun metric 1 || true
+
+            echo "Successfully changed the mode to full traffic tunneling."
             ;;
 
-        proxy)
+        false)
+            sudo kill -9 $(pidof hev-socks5-tunnel)
+
             sudo ip rule del uidrange 1001-1001 lookup 110 pref 28000
             sudo ip route del default via 192.168.1.1 dev enp9s0 metric 50 table 110
             sudo ip route del default via 172.20.0.1 dev byedpi-tun metric 1
@@ -44,8 +48,8 @@ case $1 in
     help)
         cmd_help
         ;;
-    mode)
-        cmd_mode $2
+    tun)
+        cmd_tun $2
         ;;
     *)
         echo "Invalid argument $1"
