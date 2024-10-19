b27c832a49121df71a54bd163a9d2507766b47e9
maximilionus <maximilionuss@gmail.com>
2024-10-19 05:32:19 +0300

    nvim: Remove useless API call from auto-dark ext

--- nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -3,15 +3,13 @@ return {
     opts = {
         update_interval = 1000,
         set_dark_mode = function()
-            vim.api.nvim_set_option_value("background", "dark", {})
-            vim.o.background = 'dark'
+            vim.opt.background = 'dark'
             vim.api.nvim_set_hl(0, 'Normal', { bg=none })
             vim.api.nvim_set_hl(0, 'NonText', { bg=none })
             vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
         end,
         set_light_mode = function()
-            vim.api.nvim_set_option_value("background", "light", {})
-            vim.o.background = 'light'
+            vim.opt.background = 'light'
             vim.api.nvim_set_hl(0, 'Normal', { bg=none })
             vim.api.nvim_set_hl(0, 'NonText', { bg=none })
             vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
