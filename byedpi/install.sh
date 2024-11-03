f625db8d57bcc03c4e40f21723985bf9ae80c373
maximilionus <maximilionuss@gmail.com>
2024-11-03 04:45:43 +0300

    byedpi: Update install links lock

--- byedpi/install.sh
+++ byedpi/install.sh
@@ -17,7 +17,7 @@ id -u byedpi &>/dev/null || sudo useradd -r -s /bin/false byedpi
 
 echo "-> Downloading dependencies"
 curl -L -o "$TMP_DIR/ciadpi.tar.gz" \
-    https://github.com/hufrea/byedpi/releases/download/v0.14.1/byedpi-14.1-x86_64.tar.gz
+    "https://github.com/hufrea/byedpi/releases/download/v0.15/byedpi-15-x86_64.tar.gz"
 cd "$TMP_DIR"
 tar zxvf "ciadpi.tar.gz"
 find -type f -name "ciadpi-*" -exec mv -vf {} $BIN_DIR/ciadpi \;
@@ -25,7 +25,7 @@ cd -
 chmod +x "$BIN_DIR/ciadpi"
 
 curl -L -o "$BIN_DIR/hev-socks5-tunnel" \
-    https://github.com/heiher/hev-socks5-tunnel/releases/download/2.7.4/hev-socks5-tunnel-linux-x86_64
+    "https://github.com/heiher/hev-socks5-tunnel/releases/download/2.7.4/hev-socks5-tunnel-linux-x86_64"
 chmod +x "$BIN_DIR/hev-socks5-tunnel"
 
 echo "-> Installing"
