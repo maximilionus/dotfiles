58b9fe2e2d0eb365186055bcd4db10d576a93bc3
maximilionus <maximilionuss@gmail.com>
2024-11-25 05:24:12 +0300

    zsh: Add p10k config

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,5 +3,6 @@ set -e
 
 printf "\n-> Installing the configuration...\n"
 ln -sfv "$(realpath .zshrc)" "$HOME/.zshrc"
+ln -sfv "$(realpath .p10k.zsh)" "$HOME/.p10k.zsh"
 
 printf "\nzsh configuration successfully installed, restart the shell to load the changes.\n"
