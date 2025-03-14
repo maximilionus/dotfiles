58e38934dfe7a1b7e1408dfda91764e09ec690f9
maximilionus <maximilionuss@gmail.com>
2025-03-14 14:38:22 +0300

    nvim: Commands for light-dark mode switching

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,5 +1,17 @@
-vim.cmd.colorscheme("github_dark")
-vim.api.nvim_set_hl(0, 'Normal', { bg=none })
-vim.api.nvim_set_hl(0, 'NonText', { bg=none })
-vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg=none })
-vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
+function colorscheme_set_dark()
+    vim.cmd.colorscheme("github_dark")
+    vim.api.nvim_set_hl(0, 'Normal', { bg=none })
+    vim.api.nvim_set_hl(0, 'NonText', { bg=none })
+    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg=none })
+    vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
+end
+
+function colorscheme_set_light()
+    vim.cmd.colorscheme("github_light")
+    vim.api.nvim_set_hl(0, 'Normal', { bg=none })
+    vim.api.nvim_set_hl(0, 'NonText', { bg=none })
+    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg=none })
+    vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
+end
+
+colorscheme_set_dark()
--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,3 +1,11 @@
 vim.api.nvim_create_user_command('TrimTrailWhitespace',function()
     MiniTrailspace.trim()
 end,{})
+
+vim.api.nvim_create_user_command('LightMode',function()
+    colorscheme_set_light()
+end,{})
+
+vim.api.nvim_create_user_command('DarkMode',function()
+    colorscheme_set_dark()
+end,{})
