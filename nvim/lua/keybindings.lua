a8bec40a8c3fdba7228445060fd611ae34538949
maximilionus <maximilionuss@gmail.com>
2025-04-11 05:13:29 +0300

    nvim: Remove telescope plugin

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -10,9 +10,3 @@ vim.keymap.set('n', 'Q', ':bd!<CR>')
 -- Spelling check switches
 vim.keymap.set('n', '<leader>l', ':set spell spelllang=en_us,ru_ru')
 vim.keymap.set('n', '<leader><S-l>', ':set nospell<CR>')
-
--- Telescope keybinds
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>o', builtin.find_files, {})
-vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
