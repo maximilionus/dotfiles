60684be5c6310ec385dc4250f658283b45b6b43f
maximilionus <maximilionuss@gmail.com>
2025-05-20 18:19:03 +0300

    nvim: XDG-based colorscheme switching

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,17 +1,49 @@
-function colorscheme_set_dark()
-    vim.cmd.colorscheme("github_dark")
+local function transparent_bg()
     vim.api.nvim_set_hl(0, 'Normal', { bg=none })
     vim.api.nvim_set_hl(0, 'NonText', { bg=none })
     vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg=none })
     vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
+end
+
+function colorscheme_set_dark()
+    vim.cmd.colorscheme("github_dark")
+    transparent_bg()
 end
 
 function colorscheme_set_light()
     vim.cmd.colorscheme("github_light")
-    vim.api.nvim_set_hl(0, 'Normal', { bg=none })
-    vim.api.nvim_set_hl(0, 'NonText', { bg=none })
-    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg=none })
-    vim.api.nvim_set_hl(0, 'StatusLine', { bg=none })
+    transparent_bg()
 end
 
-colorscheme_set_dark()
+function colorscheme_set_xdg()
+    local handle = io.popen([[gdbus call --session \
+    --dest org.freedesktop.portal.Desktop \
+    --object-path /org/freedesktop/portal/desktop \
+    --method org.freedesktop.portal.Settings.Read \
+    "org.freedesktop.appearance" "color-scheme"]])
+
+    local result = handle and handle:read("*a")
+    if handle then handle:close() end
+
+    local theme = "dark"
+
+    if result then
+        local value = result:match("uint32 (%d)")
+        if value == "0" then
+            theme = "light"
+        elseif value == "1" then
+            theme = "dark"
+        elseif value == "2" then
+            theme = "dark"
+        end
+    end
+
+    if theme == "light" then
+        colorscheme_set_light()
+    else
+        colorscheme_set_dark()
+    end
+end
+
+
+colorscheme_set_xdg()
--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -2,10 +2,14 @@ vim.api.nvim_create_user_command('TrimTrailWhitespace',function()
     MiniTrailspace.trim()
 end,{})
 
-vim.api.nvim_create_user_command('LightMode',function()
+vim.api.nvim_create_user_command('LightTheme',function()
     colorscheme_set_light()
 end,{})
 
-vim.api.nvim_create_user_command('DarkMode',function()
+vim.api.nvim_create_user_command('DarkTheme',function()
     colorscheme_set_dark()
+end,{})
+
+vim.api.nvim_create_user_command('SystemTheme',function()
+    colorscheme_set_xdg()
 end,{})
