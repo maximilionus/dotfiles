2ef8231891ef1aaf5f6a0ca348344d7df0cd7364
maximilionus <maximilionuss@gmail.com>
2025-08-27 19:39:09 +0300

    nvim: Remove mini plugin

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -1,7 +1,3 @@
-vim.api.nvim_create_user_command('TrimTrailWhitespace',function()
-    MiniTrailspace.trim()
-end,{})
-
 vim.api.nvim_create_user_command('LightTheme',function()
     colorscheme_set_light()
 end,{})
--- nvim/.config/nvim/lua/plugins/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'echasnovski/mini.nvim',
-    version = '*',
-    config = function()
-        require('mini.align').setup()      -- Align text with 'ga'
-        require('mini.comment').setup()    -- Comment code with 'gc'
-        require('mini.trailspace').setup() -- Trail whitespace cleanup
-    end,
-}
